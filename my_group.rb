977fb4838f569b5ed3ecea9e7d0705fca8aaf3de
flis_ann <ann.flismark@gmail.com>
2020-10-06 15:26:34 +0200

    changed puts

--- my_group.rb
+++ my_group.rb
@@ -4,6 +4,6 @@ person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
 person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
 
 my_group.each do |my_group|
-puts "#{name} is a #{age} year old #{gender}"
+puts #{name} "is a "#{age}" year old "#{gender}
 end
 
