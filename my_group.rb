97a7a9914af563bdb18dc4fac4655c4e6a665fc1
flis_ann <ann.flismark@gmail.com>
2020-10-06 15:18:32 +0200

    added a puts

--- my_group.rb
+++ my_group.rb
@@ -1,4 +1,7 @@
 my_group = []
-person1 = {:name=>"",:gender=>"", :age=>""}
-person2 = {:name=>"",:gender=>"", :age=>""}
-person3 = {:name=>"",:gender=>"", :age=>""}
+person1 = {:name=>"Ann",:gender=>"Female", :age=>"40"}
+person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
+person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
+
+puts "#{name} is a #{age} year old #{gender}"
+
