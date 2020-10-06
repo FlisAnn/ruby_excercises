a86358998363d422b23b89cd505c96cfcdc82879
flis_ann <ann.flismark@gmail.com>
2020-10-06 15:23:12 +0200

    added .each do

--- my_group.rb
+++ my_group.rb
@@ -3,5 +3,7 @@ person1 = {:name=>"Ann",:gender=>"Female", :age=>"40"}
 person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
 person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
 
+my_group.each do |my_group|
 puts "#{name} is a #{age} year old #{gender}"
+end
 
