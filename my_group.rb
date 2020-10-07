a5ece0312eeb9f80ffb4022e8b61d5caea570b90
flis_ann <ann.flismark@gmail.com>
2020-10-07 13:09:34 +0200

    change back to array and added [] to puts

--- my_group.rb
+++ my_group.rb
@@ -3,7 +3,7 @@ person1 = {:name=>"Ann",:gender=>"Female", :age=>"40"}
 person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
 person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
 
-my_group.each do |my_group|
+my_group.each do |key, value|
 puts "#{person1[name]} is a #{person1[age]} year old #{person1[gender]}"
 end
 
