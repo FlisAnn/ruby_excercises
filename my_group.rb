e0ac1ddea7506938b5a692747265d7b443586a0d
flis_ann <ann.flismark@gmail.com>
2020-10-07 13:31:37 +0200

    kommenterade bort kod

--- my_group.rb
+++ my_group.rb
@@ -3,7 +3,7 @@ person1 = {name: 'Ann', gender: 'Female', age: '40'}
 # person2 = { name: ´Pip´, gender: ´Female´, age: ´5´}
 # person3 = { name: ´Mio´, gender: ´Male´, age: ´5´}
 
-person1.each do |name, gender, age|
+person1.each do |person1|
 puts "#{name} is a #{age} year old #{gender}"
 end
 
