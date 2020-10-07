df20218e0669077bddb023ea81ca0577ad8867bc
flis_ann <ann.flismark@gmail.com>
2020-10-07 12:50:44 +0200

    change to person1

--- my_group.rb
+++ my_group.rb
@@ -3,7 +3,7 @@ person1 = {:name=>"Ann",:gender=>"Female", :age=>"40"}
 person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
 person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
 
-my_group.each do |my_group|
+person1.each do |person1|
 puts "#{name} is a #{age} year old #{gender}"
 end
 
