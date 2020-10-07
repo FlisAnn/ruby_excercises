243b33905a6fb259779d0c102c561b6e99bf5b2c
flis_ann <ann.flismark@gmail.com>
2020-10-07 12:48:22 +0200

    changed from array to hash

--- my_group.rb
+++ my_group.rb
@@ -1,9 +1,9 @@
-my_group = []
+my_group = {}
 person1 = {:name=>"Ann",:gender=>"Female", :age=>"40"}
 person2 = {:name=>"Pip",:gender=>"Female", :age=>"5"}
 person3 = {:name=>"Mio",:gender=>"Male", :age=>"5"}
 
 my_group.each do |my_group|
-puts #{name} "is a "#{age}" year old "#{gender}
+puts "#{name} is a #{age} year old #{gender}"
 end
 
