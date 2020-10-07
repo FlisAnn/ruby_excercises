0d82a232b4c13e161f71411805d5ebd253e6408f
flis_ann <ann.flismark@gmail.com>
2020-10-07 13:27:25 +0200

    kommenterade bort kod

--- my_group.rb
+++ my_group.rb
@@ -1,9 +1,9 @@
-my_group = [
-person1 = { name: "Ann", gender: "Female", age: "40"}
-# person2 = { name: "Pip", gender: "Female", age: "5"}
-# person3 = { name: "Mio", gender: "Male", age: "5"}
-]
-my_group.each do |key, value|
-puts "#{person1[name]} is a #{person1[age]} year old #{person1[gender]}"
+
+person1 = {name: 'Ann', gender: 'Female', age: '40'}
+# person2 = { name: ´Pip´, gender: ´Female´, age: ´5´}
+# person3 = { name: ´Mio´, gender: ´Male´, age: ´5´}
+
+person1.each do |key, value|
+puts "#{name} is a #{age} year old #{gender}"
 end
 
